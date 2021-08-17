903a32a5787d75dc8541e3a2c7f9eac0248e90c4
ceco27 <cvetanivanov2002@gmail.com>
2021-08-17 16:38:11 +0300

    added updating and deleting people

--- src/main/java/com/ceco/javatestproject/dao/FakePersonDataAccessService.java
+++ src/main/java/com/ceco/javatestproject/dao/FakePersonDataAccessService.java
@@ -15,20 +15,30 @@ public class FakePersonDataAccessService implements PersonDao {
 
     @Override
     public int deletePersonById(UUID id) {
-        // TODO Auto-generated method stub
-        return 0;
+        Optional<Person> personMaybe = selectPersonById(id);
+        if(personMaybe.isEmpty()) {
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        // TODO Auto-generated method stub
         return DB.stream().filter(person -> person.getId().equals(id)).findFirst();
     }
 
     @Override
-    public int updatePersonById(UUID id, Person person) {
-        // TODO Auto-generated method stub
-        return 0;
+    public int updatePersonById(UUID id, Person update) {
+        return selectPersonById(id).map(p -> {
+            int indexOfPersonToUpdate = DB.indexOf(p);
+            if(indexOfPersonToUpdate >= 0) {
+                DB.set(indexOfPersonToUpdate, new Person(id, update.getName(), update.getAge()));
+                return 1;
+            }
+            return 0;
+        })
+        .orElse(0);
     }
 
     private static List<Person> DB = new ArrayList<>();
